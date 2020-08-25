6458d2a3359fab74ddd9ea94cfa3fc71d02b56e1
kantarow <kanta7306@gmail.com>
2020-08-26 04:33:41 +0900

    特定のユーザーのツイートから俳句を探す

--- main.rb
+++ main.rb
@@ -10,3 +10,27 @@ client = Twitter::REST::Client.new do |config|
   config.access_token        = ENV['ACCESS_TOKEN']
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
+
+TARGET_USERS = [
+  "kantarow_",
+]
+
+TARGET_USERS.each do |user|
+  tweets = client.user_timeline(user)
+
+  tweets.each do |tweet|
+    text = tweet.full_text
+    mogamigawa = Mogamigawa.new text
+    haiku = []
+
+    begin
+      haiku << mogamigawa.consume(5).join
+      haiku << mogamigawa.consume(7).join
+      haiku << mogamigawa.consume(5).join
+    rescue RangeError
+      next
+    end
+
+    p haiku
+  end
+end
