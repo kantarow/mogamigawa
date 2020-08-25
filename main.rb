30da9218ec8a217ea712571a9a0da6460bbe3e31
kantarow <kanta7306@gmail.com>
2020-08-26 04:03:04 +0900

    TwitterBotを初期化

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 require_relative "lib/mogamigawa"
+require 'dotenv'
+require 'twitter'
 
-mogamigawa = Mogamigawa.new "五月雨をあつめて早し最上川"
+Dotenv.load
 
-puts mogamigawa.consume(5).join
-puts mogamigawa.consume(7).join
-puts mogamigawa.consume(5).join
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['API_KEY']
+  config.consumer_secret     = ENV['API_KEY_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
